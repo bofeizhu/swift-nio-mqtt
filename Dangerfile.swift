574e65cc80f71b339d65162219bad33a3098bed2
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-17 23:17:46 -0700

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,5 +7,7 @@ if violations.isEmpty {
     message("All checks passed. Good job!!!")
 } else {
     let violationString = violations.count > 1 ? "violations" : "violation"
-    warn("Found \(violations.count) \(violationString)! You can run `swiftlint autocorrect` in terminal to fix minor violations.")
+    warn(
+        "Found \(violations.count) \(violationString)!" +
+        "You can run `swiftlint autocorrect` in terminal to fix minor violations.")
 }
