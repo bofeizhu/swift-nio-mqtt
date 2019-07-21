e2e7636f0be99ae1f692a1bcfefe6d8a18c42aea
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-21 07:44:04 -0700

    unit test binary data

--- Tests/ByteBufferTests.swift
+++ Tests/ByteBufferTests.swift
@@ -15,6 +15,7 @@ import NIO
 // swiftlint:disable force_try
 
 class ByteBufferTest: XCTestCase {
+
     private let allocator = ByteBufferAllocator()
     private var buffer: ByteBuffer! = nil
 
@@ -33,17 +34,7 @@ class ByteBufferTest: XCTestCase {
         super.tearDown()
     }
 
-    // MARK: UTF-8 Encoded String
-
-    func testMQTTStringWrite() {
-        let string = "test"
-        let written = try! buffer.writeMQTTString(string)
-        XCTAssertEqual(written, string.count + 2)
-        let count: UInt16 = buffer.readInteger()!
-        XCTAssertEqual(Int(count), string.count)
-        let readString = buffer.readString(length: Int(count))
-        XCTAssertEqual(string, readString)
-    }
+    // MARK: MQTT UTF-8 Encoded String
 
     func testMQTTStringRead() {
         let string = "test"
@@ -55,7 +46,56 @@ class ByteBufferTest: XCTestCase {
         XCTAssertEqual(buffer.readableBytes, 0)
     }
 
+    func testMQTTStringWrite() {
+        let string = "test"
+        let written = try! buffer.writeMQTTString(string)
+        XCTAssertEqual(written, string.count + 2)
+        let count: UInt16 = buffer.readInteger()!
+        XCTAssertEqual(Int(count), string.count)
+        let readString = buffer.readString(length: Int(count))
+        XCTAssertEqual(string, readString)
+    }
+
     // MARK: - VInt
+
+    func testVIntRead() {
+
+        // Test 0
+        buffer.writeByte(0)
+        var integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 0))
+        XCTAssertEqual(buffer.readableBytes, 0)
+
+        // Test 127
+        buffer.writeByte(127)
+        integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 127))
+        XCTAssertEqual(buffer.readableBytes, 0)
+
+        // Test 128
+        buffer.writeBytes([UInt8(0x80), UInt8(0x01)])
+        integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 128))
+        XCTAssertEqual(buffer.readableBytes, 0)
+
+        // Test 16,383
+        buffer.writeBytes([UInt8(0xFF), UInt8(0x7F)])
+        integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 16383))
+        XCTAssertEqual(buffer.readableBytes, 0)
+
+        // Test 2,097,152
+        buffer.writeBytes([UInt8(0x80), UInt8(0x80), UInt8(0x80), UInt8(0x01)])
+        integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 2097152))
+        XCTAssertEqual(buffer.readableBytes, 0)
+
+        // Test 268,435,455
+        buffer.writeBytes([UInt8(0xFF), UInt8(0xFF), UInt8(0xFF), UInt8(0x7F)])
+        integer = buffer.readVariableByteInteger()
+        XCTAssertEqual(integer, VInt(value: 268435455))
+        XCTAssertEqual(buffer.readableBytes, 0)
+    }
 
     func testVIntWrite() {
 
@@ -102,42 +142,26 @@ class ByteBufferTest: XCTestCase {
         XCTAssertEqual(data, [UInt8(0xFF), UInt8(0xFF), UInt8(0xFF), UInt8(0x7F)])
     }
 
-    func testVIntRead() {
+    // MARK: MQTT Binary Data
 
-        // Test 0
-        buffer.writeByte(0)
-        var integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 0))
+    func testBinaryDataRead() {
+        let bytes: [UInt8] = [0x00, 0x01, 0x02, 0x03]
+        let data = Data(bytes)
+        buffer.writeInteger(UInt16(4))
+        buffer.writeBytes(bytes)
+        let dataRead = buffer.readMQTTBinaryData()
+        XCTAssertEqual(data, dataRead)
         XCTAssertEqual(buffer.readableBytes, 0)
+    }
 
-        // Test 127
-        buffer.writeByte(127)
-        integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 127))
-        XCTAssertEqual(buffer.readableBytes, 0)
-
-        // Test 128
-        buffer.writeBytes([UInt8(0x80), UInt8(0x01)])
-        integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 128))
-        XCTAssertEqual(buffer.readableBytes, 0)
-
-        // Test 16,383
-        buffer.writeBytes([UInt8(0xFF), UInt8(0x7F)])
-        integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 16383))
-        XCTAssertEqual(buffer.readableBytes, 0)
-
-        // Test 2,097,152
-        buffer.writeBytes([UInt8(0x80), UInt8(0x80), UInt8(0x80), UInt8(0x01)])
-        integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 2097152))
-        XCTAssertEqual(buffer.readableBytes, 0)
-
-        // Test 268,435,455
-        buffer.writeBytes([UInt8(0xFF), UInt8(0xFF), UInt8(0xFF), UInt8(0x7F)])
-        integer = buffer.readVariableByteInteger()
-        XCTAssertEqual(integer, VInt(value: 268435455))
-        XCTAssertEqual(buffer.readableBytes, 0)
+    func testBinaryDataWrite() {
+        let bytes: [UInt8] = [0x00, 0x01, 0x02, 0x03]
+        let data = Data(bytes)
+        let written = try! buffer.writeMQTTBinaryData(data)
+        XCTAssertEqual(written, 6)
+        let count: UInt16 = buffer.readInteger()!
+        XCTAssertEqual(Int(count), 4)
+        let bytesWritten = buffer.readBytes(length: Int(count))
+        XCTAssertEqual(bytes, bytesWritten)
     }
 }
