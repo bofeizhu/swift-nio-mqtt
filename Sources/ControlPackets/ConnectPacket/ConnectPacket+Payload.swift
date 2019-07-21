e236e154487da4bd429dcd99374e83907b462d0a
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-21 16:30:48 -0700

    lint

--- Sources/ControlPackets/ConnectPacket/ConnectPacket+Payload.swift
+++ Sources/ControlPackets/ConnectPacket/ConnectPacket+Payload.swift
@@ -13,11 +13,13 @@ extension ConnectPacket: PayloadPacket {
     /// The Payload for the CONNECT Packet
     struct Payload {
 
-        /// The Will Message consists of the Will Properties, Will Topic, and Will Payload fields in the CONNECT Payload.
-        /// The Will Message MUST be published after the Network Connection is subsequently closed and
-        /// either the Will Delay Interval has elapsed or the Session ends, unless the Will Message
-        /// has been deleted by the Server on receipt of a DISCONNECT packet with Reason Code 0x00 (Normal disconnection)
-        /// or a new Network Connection for the ClientID is opened before the Will Delay Interval has elapsed
+        /// The Will Message consists of the Will Properties, Will Topic,
+        /// and Will Payload fields in the CONNECT Payload.
+        /// The Will Message MUST be published after the Network Connection is subsequently closed
+        /// and either the Will Delay Interval has elapsed or the Session ends, unless the Will Message
+        /// has been deleted by the Server on receipt of a DISCONNECT packet
+        /// with Reason Code 0x00 (Normal disconnection) or a new Network Connection for
+        /// the ClientID is opened before the Will Delay Interval has elapsed
         struct WillMessage {
 
             /// Properties
