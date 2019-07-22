5a00fb40c76cbe8167a5d0b6533a52dc276f46b9
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-21 22:08:54 -0700

    remove deep nesting

--- Sources/ControlPackets/ConnectPacket/ConnectPacket+Payload.swift
+++ Sources/ControlPackets/ConnectPacket/ConnectPacket+Payload.swift
@@ -13,25 +13,6 @@ extension ConnectPacket: PayloadPacket {
     /// The Payload for the CONNECT Packet
     struct Payload {
 
-        /// The Will Message consists of the Will Properties, Will Topic,
-        /// and Will Payload fields in the CONNECT Payload.
-        /// The Will Message MUST be published after the Network Connection is subsequently closed
-        /// and either the Will Delay Interval has elapsed or the Session ends, unless the Will Message
-        /// has been deleted by the Server on receipt of a DISCONNECT packet
-        /// with Reason Code 0x00 (Normal disconnection) or a new Network Connection for
-        /// the ClientID is opened before the Will Delay Interval has elapsed
-        struct WillMessage {
-
-            /// Properties
-            var properties: [Property]
-
-            /// Topic
-            var topic: String
-
-            /// Payload
-            var payload: Data
-        }
-
         /// Client Identifier (ClientID)
         let clientId: String
 
@@ -43,5 +24,24 @@ extension ConnectPacket: PayloadPacket {
 
         /// Password
         let password: String?
+    }
+
+    /// The Will Message consists of the Will Properties, Will Topic,
+    /// and Will Payload fields in the CONNECT Payload.
+    /// The Will Message MUST be published after the Network Connection is subsequently closed
+    /// and either the Will Delay Interval has elapsed or the Session ends, unless the Will Message
+    /// has been deleted by the Server on receipt of a DISCONNECT packet
+    /// with Reason Code 0x00 (Normal disconnection) or a new Network Connection for
+    /// the ClientID is opened before the Will Delay Interval has elapsed
+    struct WillMessage {
+
+        /// Properties
+        var properties: [Property]
+
+        /// Topic
+        var topic: String
+
+        /// Payload
+        var payload: Data
     }
 }
