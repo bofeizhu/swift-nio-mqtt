ec1a8ed131d609a5943b06f25e2c84bce0da23f3
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-21 23:01:08 -0700

    connect reason code

---
+++ Sources/ControlPackets/ConnackPacket/ConnackPacket+Payload.swift
@@ -0,0 +1,15 @@
+//
+//  ConnackPacket+Payload.swift
+//  NIOMQTT
+//
+//  Created by Bofei Zhu on 7/21/19.
+//  Copyright © 2019 HealthTap Inc. All rights reserved.
+//
+
+import struct Foundation.Data
+
+extension ConnackPacket: PayloadPacket {
+
+    /// The Payload for the CONNACK Packet
+    struct Payload {}
+}
