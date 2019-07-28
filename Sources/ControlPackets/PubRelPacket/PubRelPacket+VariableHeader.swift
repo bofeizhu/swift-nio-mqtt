292bc0bf780e98dffa371ac31eafd23126b5716f
Bofei Zhu <bofei.zhu@healthtap.com>
2019-07-28 16:00:38 -0700

    finished PUBREL

---
+++ Sources/ControlPackets/PubRelPacket/PubRelPacket+VariableHeader.swift
@@ -0,0 +1,23 @@
+//
+//  PubRelPacket+VariableHeader.swift
+//  NIOMQTT
+//
+//  Created by Bofei Zhu on 7/28/19.
+//  Copyright © 2019 HealthTap Inc. All rights reserved.
+//
+
+extension PubRelPacket: VariableHeaderPacket {
+
+    /// PUBREL Variable Header
+    struct VariableHeader: HasProperties {
+
+        /// Packet Identifier
+        let packetIdentifier: UInt16
+
+        /// Reason Code
+        let reasonCode: ReasonCode
+
+        /// Properties
+        let properties: [Property]
+    }
+}
--- Sources/ControlPackets/PubRelPacket/PubRelPacket.swift
+++ Sources/ControlPackets/PubRelPacket/PubRelPacket.swift
@@ -6,7 +6,15 @@
 //  Copyright © 2019 HealthTap Inc. All rights reserved.
 //
 
+/// PUBREL Packet – Publish release (QoS 2 delivery part 2)
 struct PubRelPacket: ControlPacketProtocol {
 
-    var fixedHeader: FixedHeader
+    // Reserved fixed header flags for PUBREL packet
+    static let flags: FixedHeaderFlags = 2
+
+    /// Fixed Header
+    let fixedHeader: FixedHeader
+
+    /// Variable Header
+    let variableHeader: VariableHeader
 }
