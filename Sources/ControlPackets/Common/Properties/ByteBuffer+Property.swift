ec631101a426a4d00d0957b5fde3cf0dcfc89660
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-11 23:42:02 -0700

    add file

---
+++ Sources/ControlPackets/Common/Properties/ByteBuffer+Property.swift
@@ -0,0 +1,13 @@
+//
+//  ByteBuffer+Property.swift
+//  NIOMQTT
+//
+//  Created by Bofei Zhu on 8/11/19.
+//  Copyright © 2019 HealthTap Inc. All rights reserved.
+//
+
+import NIO
+
+extension ByteBuffer {
+    
+}
