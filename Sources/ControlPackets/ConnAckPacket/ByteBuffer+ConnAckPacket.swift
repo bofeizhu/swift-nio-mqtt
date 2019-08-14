47edf3a85b4b22e94bce0e5517f6c6c95610c7de
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-14 15:30:10 -0700

    add file

---
+++ Sources/ControlPackets/ConnAckPacket/ByteBuffer+ConnAckPacket.swift
@@ -0,0 +1,13 @@
+//
+//  ByteBuffer+ConnAckPacket.swift
+//  NIOMQTT
+//
+//  Created by Bofei Zhu on 8/14/19.
+//  Copyright © 2019 HealthTap Inc. All rights reserved.
+//
+
+import NIO
+
+extension ByteBuffer {
+
+}
