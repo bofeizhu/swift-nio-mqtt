e5dd103fd7d36b93c8ab86e4562afdef2d2e05d6
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-19 09:19:05 -0700

    read pub packets

--- Sources/ControlPackets/PubRelPacket/ByteBuffer+PubRelPacket.swift
+++ Sources/ControlPackets/PubRelPacket/ByteBuffer+PubRelPacket.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019 HealthTap Inc. All rights reserved.
 //
 
-import Foundation
+import NIO
+
+extension ByteBuffer {
+
+    mutating func readPubRelPacket(with fixedHeader: FixedHeader) throws -> PubRelPacket {
+
+        guard
+            let packetIdentifier: UInt16 = readInteger(),
+            let reasonCodeValue = readByte(),
+            let reasonCode = PubRelPacket.ReasonCode(rawValue: reasonCodeValue)
+        else {
+            throw MQTTCodingError.malformedPacket
+        }
+
+        let properties = try readProperties()
+
+        let variableHeader = PubRelPacket.VariableHeader(
+            packetIdentifier: packetIdentifier,
+            reasonCode: reasonCode,
+            properties: properties)
+
+        return PubRelPacket(fixedHeader: fixedHeader, variableHeader: variableHeader)
+    }
+}
