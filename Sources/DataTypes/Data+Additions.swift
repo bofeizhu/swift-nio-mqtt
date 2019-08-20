ecd1384caeea21769d3651ffddbb7528c6afa835
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-20 08:31:03 -0700

    nit

--- Sources/DataTypes/Data+Additions.swift
+++ Sources/DataTypes/Data+Additions.swift
@@ -11,6 +11,9 @@ import struct Foundation.Data
 extension Data {
 
     /// Byte count for MQTT binary data, including the Two Byte Integer length field
+    ///
+    /// In MQTT, Binary Data is represented by a Two Byte Integer length which indicates the number of data bytes,
+    /// followed by that number of bytes. Thus, the length of Binary Data is limited to the range of 0 to 65,535 Bytes.
     var mqttByteCount: Int {
         return count + 2
     }
