66903e31390b6c8728243856565865be864afad7
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-21 15:08:22 -0700

    add control packet read cases

--- Sources/ControlPackets/ByteBuffer+ControlPacket.swift
+++ Sources/ControlPackets/ByteBuffer+ControlPacket.swift
@@ -17,6 +17,46 @@ extension ByteBuffer {
             let packet = try readConnAckPacket(with: fixedHeader)
             return .connAck(packet: packet)
 
+        case .publish:
+            let packet = try readPublishPacket(with: fixedHeader)
+            return .publish(packet: packet)
+
+        case .pubAck:
+            let packet = try readPubAckPacket(with: fixedHeader)
+            return .pubAck(packet: packet)
+
+        case .pubRec:
+            let packet = try readPubRecPacket(with: fixedHeader)
+            return .pubRec(packet: packet)
+
+        case .pubRel:
+            let packet = try readPubRelPacket(with: fixedHeader)
+            return .pubRel(packet: packet)
+
+        case .pubComp:
+            let packet = try readPubCompPacket(with: fixedHeader)
+            return .pubComp(packet: packet)
+
+        case .subAck:
+            let packet = try readSubAckPacket(with: fixedHeader)
+            return .subAck(packet: packet)
+
+        case .unsubAck:
+            let packet = try readUnsubAckPacket(with: fixedHeader)
+            return .unsubAck(packet: packet)
+
+        case .pingResp:
+            let packet = PingRespPacket(fixedHeader: fixedHeader)
+            return .pingResp(packet: packet)
+
+        case .disconnect:
+            let packet = try readDisconnectPacket(with: fixedHeader)
+            return .disconnect(packet: packet)
+
+        case .auth:
+            let packet = try readAuthPacket(with: fixedHeader)
+            return .auth(packet: packet)
+
         default:
             throw MQTTCodingError.malformedPacket
         }
