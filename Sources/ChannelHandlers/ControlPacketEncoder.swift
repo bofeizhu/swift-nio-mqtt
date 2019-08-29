2540e93b241aebee6ea74ef084bb22a35091e991
Bofei Zhu <bofei.zhu@healthtap.com>
2019-08-29 11:25:31 -0700

    control packet encoder

---
+++ Sources/ChannelHandlers/ControlPacketEncoder.swift
@@ -0,0 +1,17 @@
+//
+//  ControlPacketEncoder.swift
+//  NIOMQTT
+//
+//  Created by Bofei Zhu on 8/29/19.
+//  Copyright © 2019 HealthTap Inc. All rights reserved.
+//
+
+import NIO
+
+final class ControlPacketEncoder: MessageToByteEncoder {
+    typealias OutboundIn = ControlPacket
+
+    func encode(data: ControlPacket, out: inout ByteBuffer) throws {
+        try out.write(data)
+    }
+}
--- Sources/ControlPackets/ByteBuffer+ControlPacket.swift
+++ Sources/ControlPackets/ByteBuffer+ControlPacket.swift
@@ -63,6 +63,7 @@ extension ByteBuffer {
         }
     }
 
+    @discardableResult
     mutating func write(_ controlPacket: ControlPacket) throws -> Int {
 
         switch controlPacket {
